5dc58abb621310c71db9a28c60bea2a19ac5acfa
Bras <adamzieli97@gmail.com>
2021-01-08 15:27:03 +0100

    Initialization

--- src/components/add-new-post/Editor.js
+++ src/components/add-new-post/Editor.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactQuill from "react-quill";
 import { Card, CardBody, Form, FormInput } from "shards-react";
+import ImageUploader from 'react-images-upload';
 
 import "react-quill/dist/quill.snow.css";
 import "../../assets/quill.css";
 
-const Editor = () => (
+
+
+function onDrop(picture) {
+  this.setState({
+      pictures: this.state.pictures.concat(picture),
+  });
+}
+
+const Editor = () => (  
   <Card small className="mb-3">
     <CardBody>
       <Form className="add-new-post">
         <FormInput size="lg" className="mb-3" placeholder="Your Post Title" />
         <ReactQuill className="add-new-post__editor mb-1" />
+      <ImageUploader
+                withIcon={true}
+                buttonText='Wybierz zdjęcie'
+                onChange={onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
       </Form>
     </CardBody>
   </Card>
--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -1,18 +1,27 @@
 export default function() {
   return [
     {
-      title: "Blog Dashboard",
-      to: "/blog-overview",
-      htmlBefore: '<i class="material-icons">edit</i>',
-      htmlAfter: ""
+      title: "Profil trenera",
+      htmlBefore: '<i class="material-icons">person</i>',
+      to: "/user-profile-lite",
     },
     {
-      title: "Blog Posts",
+      title: "Posty",
       htmlBefore: '<i class="material-icons">vertical_split</i>',
       to: "/blog-posts",
     },
     {
-      title: "Add New Post",
+      title: "Nowy post",
+      htmlBefore: '<i class="material-icons">note_add</i>',
+      to: "/add-new-post",
+    },
+    {
+      title: "Cenniki",
+      htmlBefore: '<i class="material-icons">note_add</i>',
+      to: "/add-new-post",
+    },
+    {
+      title: "Wydarzenia",
       htmlBefore: '<i class="material-icons">note_add</i>',
       to: "/add-new-post",
     },
@@ -20,21 +29,6 @@ export default function() {
       title: "Forms & Components",
       htmlBefore: '<i class="material-icons">view_module</i>',
       to: "/components-overview",
-    },
-    {
-      title: "Tables",
-      htmlBefore: '<i class="material-icons">table_chart</i>',
-      to: "/tables",
-    },
-    {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
-    },
-    {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
     }
   ];
 }
